ca0d5e6c9039b1cd5483268a61a16d1d49f26c87
Lipa03 <lipamannapersonal93@gmail.com>
2025-09-07 23:56:31 +0530

    setup in-progress

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
 import express from "express";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: './.env'
+})
+
 const app = express();
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8001, () => {
+        console.log(`Server is running on port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((error) => {
+    console.log('Error:', error);
+    
+})
 
 // (
 //     async () => {
