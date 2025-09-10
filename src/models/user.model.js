0014f88481fb312844f48245c42bf9fe26a391a7
Lipa03 <lipamannapersonal93@gmail.com>
2025-09-11 01:16:42 +0530

    setup in progress

---
+++ src/models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    avatar: {
+        type: String, //Cloudnary URL
+        required: true
+    },
+    coverImage: {
+        type: String, //Cloudnary URL
+        required: false
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String,
+    },
+},
+    {
+        timestamps: true,
+    })
+
+export const User = mongoose.model('User', userSchema)
